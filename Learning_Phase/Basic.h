ecd2539c221bb496d13d4f4a8376ad334e6e02c2
braydenleung-Git <62462739+braydenleung-Git@users.noreply.github.com>
2023-05-30 18:20:02 +0000

    Finished W3’s code

---
+++ Learning_Phase/Basic.h
@@ -0,0 +1,14 @@
+/*
+This class is the basic tool kit that include all things needed 
+*/
+#include <iostream>
+using namespace std;
+
+class Basic{
+    string N = "";
+    void stopper();
+};
+
+void Basic::stopper(){
+    cin>> N;
+}
