7369b753944cda20c01475e92dc7163e9e7ee536
braydenleung-Git <brayden.leung@gmail.com>
2023-10-23 22:59:11 -0400

    modified:   .DS_Store 	modified:   Learning_Phase/.DS_Store 	modified:   Learning_Phase/W1/D3 	new file:   Learning_Phase/W1/D3.dSYM/Contents/Info.plist 	new file:   Learning_Phase/W1/D3.dSYM/Contents/Resources/DWARF/D3 	new file:   Learning_Phase/W1/D3.dSYM/Contents/Resources/Relocations/x86_64/D3.yml 	deleted:    Learning_Phase/W3/D2

---
+++ Learning_Phase/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+#include <cmath>
+using namespace std;
+int main()
+{
+    
+    cout<<" Enter bla bla blasdghasd : ";
+    int variable = 0;
+    cin>>variable;
+
+    return 0;
+}
